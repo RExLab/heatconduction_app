fb7907ee828b50f19da5c4316321398f7657e3bb
João Paulo Cardoso de Lima <joao.pcl@grad.ufsc.br>
2016-05-12 15:25:02 -0300

    second commit

--- apps.js
+++ apps.js
@@ -10,85 +10,78 @@ var port = 80;
 app.use(express.static(__dirname + '/public'));
 
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-  
+    console.log('Server listening at port %d', port);
+
 });
 
 
 var configured = false, authenticated = false;
 
 io.on('connection', function (socket) {
-  var password  = ""; 
-  var duration = 0, interval=0;
-  var step = 2;
-
-  
-  function SendMessage(socket){
-	  
-	  if (duration > 0 ){
-		var data = panel.getvalues();
-		console.log(data);
-		socket.emit('data received', data);
-		duration --; 
-	  }else{
-    	clearInterval(interval);
-		panel.digital(0);
-	  }
-	  
-  }
-  
-  socket.on('new connection', function(data){
-	 // fazer acesso ao rlms para autenticar
-     //password = data.pass;
-	 console.log('new connection' + data);
-
-	  authenticated = true;
-	  configured = panel.setup();
-	  if(configured){
-		  panel.run();
-	  }else{
-		  panel.exit();
-		  panel.setup();
-  		  panel.run();
-	  }
-      
-  });
-  
-  
-  socket.on('start', function (data) {
-	console.log(data);
-	if(data.sw > 0 ){
-		panel.digital(1);
-		clearInterval(interval);
-		duration = data.duration;
-		step = ( data.step > 1) ? data.step : 2;
-		interval = setInterval(function(){ SendMessage(socket); }, step*1000);  
-		SendMessage(socket);
-		
-	}else{
-		panel.digital(0);
-	    clearInterval(interval);		
-	}
-	  
-  });
+    var password = "";
+    var duration = 0, interval = 0;
+    var step = 2;
 
 
-  socket.on('disconnect', function () {
-    clearInterval(interval);
-	console.log('disconnected');
-	panel.digital(0);
-	setTimeout(function(){
-		
-		if (configured) {
-			configured = authenticated = false;
-			panel.exit();
-		}else{
-			panel.setup();
-			panel.run();
-			panel.exit();
-		}
-	}, 1000);
-  });
-  
-  
+    function SendMessage(socket) {
+
+        if (duration > 0) {
+            var data = panel.getvalues();
+            console.log(data);
+            socket.emit('data received', data);
+            duration--;
+        } else {
+            clearInterval(interval);
+            panel.digital(0);
+        }
+
+    }
+
+    socket.on('new connection', function (data) {
+        // fazer acesso ao rlms para autenticar
+        //password = data.pass;
+        console.log('new connection' + data);
+        authenticated = true;
+    });
+
+
+    socket.on('start', function (data) {
+        if (!configured) {
+            configured = panel.setup();
+            if (configured)
+                console.log("thread : " + panel.run());
+            else
+                console.log('Emit error here');
+        }
+        console.log(data);
+        if (data.sw > 0) {
+            panel.digital(1);
+            clearInterval(interval);
+            duration = data.duration;
+            step = (data.step > 1) ? data.step : 2;
+            interval = setInterval(function () {
+                SendMessage(socket);
+            }, step * 1000);
+            SendMessage(socket);
+
+        } else {
+            panel.digital(0);
+            clearInterval(interval);
+        }
+    });
+
+
+    socket.on('disconnect', function () {
+        clearInterval(interval);
+        console.log('disconnected');
+        //panel.digital(0);
+        setTimeout(function () {
+            if (configured) {
+                configured = authenticated = false;
+                panel.exit();
+            }             
+        }, 1000);
+    });
+
+
 });
